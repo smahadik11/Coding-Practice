134f81af190d8c041b101d1db08179ade1f6ece6
smahadik11 <sudha.mahadik11@gmail.com>
2017-06-08 14:35:18 -0700

    add existing file

--- Problem2.c
+++ Problem2.c
@@ -1,34 +1,5 @@
 Source : Leetcode.com
 
-Validate if a given string is numeric.
-
-Some examples:
-"0" => true
-" 0.1 " => true
-"abc" => false
-"1 a" => false
-"2e10" => true
-Note: It is intended for the problem statement to be ambiguous. You should gather all requirements up front before implementing one.
-
-bool isNumber(char* s) {
-    
-    bool isDigit = 0;
-    bool isLetter = 0;
-    unsigned int i = 0;  
-    for(i=0;i<strlen(s);i++)
-    {
-        if(s[i]>='0' && s[i]<='9')
-            isDigit = 1;
-        else
-           isLetter = 1;
- 
-    }
-    if(isLetter == 0)
-        return 1;
-    return 0;
-}
-
-
 Remove Duplicate Letters
 Difficulty:Hard
 
